5e3e7a4797e6016ec13a408719359fe50cfef1cc
skarmaotsal8-star <skarmaotsal8@gmail.com>
2025-10-02 22:55:22 +0530

    Fix GitHub Pages routing with _redirects and basename

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import HomePage from './HomePage';
-import SkillDetail from './SkillDetail';
-import Profile from './Profile';
-import Requests from './Requests';
-import Responses from './Responses';
-import Premium from './Premium';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-function App() {
-  return (
-    <Routes>
-      {/* Login page */}
-      <Route path="/" element={<Login />} />
-
-      {/* Home page */}
-      <Route path="/home" element={<HomePage />} />
-
-      {/* Skill detail page */}
-      <Route path="/skill/:id" element={<SkillDetail />} />
-
-      {/* Profile page */}
-      <Route path="/profile" element={<Profile />} />
-
-      {/* Requests page */}
-      <Route path="/requests" element={<Requests />} />
-
-      {/* Responses page */}
-      <Route path="/responses" element={<Responses />} />
-
-      {/* Premium page */}
-      <Route path="/premium" element={<Premium />} />
-
-      {/* Catch-all 404 */}
-      <Route path="*" element={<div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        404 Page Not Found
-      </div>} />
-    </Routes>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter basename="/myapp">
+    <App />
+  </BrowserRouter>
+);
